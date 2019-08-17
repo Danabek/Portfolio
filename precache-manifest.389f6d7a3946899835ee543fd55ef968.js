7906c18a27354e38a0ad9fccc78fb0da85d2ecd7
Danabek <d.sakhmet@gmail.com>
2019-08-17 17:13:07 +0600

    Updates

---
+++ precache-manifest.389f6d7a3946899835ee543fd55ef968.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a6fa994d9b2fb66a8b92b605798958",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "87ceb63182c5ad411340",
+    "url": "/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "978e71844107db66404c",
+    "url": "/portfolio/static/css/main.fafe89b7.chunk.css"
+  },
+  {
+    "revision": "87ceb63182c5ad411340",
+    "url": "/portfolio/static/js/2.1d50aafd.chunk.js"
+  },
+  {
+    "revision": "978e71844107db66404c",
+    "url": "/portfolio/static/js/main.e7de09a3.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "fb8e72ea47642b4e8deb6abfe1a61889",
+    "url": "/portfolio/static/media/Almatinka.fb8e72ea.jpg"
+  },
+  {
+    "revision": "4f86ef12f713ddfb134577d10f51f1a7",
+    "url": "/portfolio/static/media/LinkedIn-icon.4f86ef12.png"
+  },
+  {
+    "revision": "5673da52c98bb6cb33ada5aaf649703e",
+    "url": "/portfolio/static/media/Roboto.5673da52.ttf"
+  },
+  {
+    "revision": "392c69bdd859159825cd9fda3d077055",
+    "url": "/portfolio/static/media/hh-logo.392c69bd.png"
+  },
+  {
+    "revision": "9fe9f08deba669e1b1f2c605101d713c",
+    "url": "/portfolio/static/media/me2.9fe9f08d.png"
+  }
+]);
